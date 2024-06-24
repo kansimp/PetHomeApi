76556ae316bda32636432fc5a01f832b5712e9eb
kansimp <khoa21062003@gmail.com>
2024-06-24 16:41:50 +0700

    Booking Service API

--- src/api/controllers/serviceRecordController.js
+++ src/api/controllers/serviceRecordController.js
@@ -16,7 +16,41 @@ const createServiceRecord = async (req, res) => {
         });
     }
 };
+const cancelServiceRecord = async (req, res) => {
+    try {
+        let data = await serviceRecordService.cancelServiceRecord(req.body);
+        return res.status(200).json({
+            status: data.status,
+            message: data.message,
+            data: data.data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            message: 'error from server',
+            data: '',
+        });
+    }
+};
+const confirmServiceRecord = async (req, res) => {
+    try {
+        let data = await serviceRecordService.confirmServiceRecord(req.body);
+        return res.status(200).json({
+            status: data.status,
+            message: data.message,
+            data: data.data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            message: 'error from server',
+            data: '',
+        });
+    }
+};
 
 module.exports = {
     createServiceRecord,
+    cancelServiceRecord,
+    confirmServiceRecord,
 };
